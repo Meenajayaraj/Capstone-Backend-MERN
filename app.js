0b397567db29a1e318672506a32eb7fd372f17b8
sundharaj2517@gmail.com <sundharaj2517@gmail.com>
2025-02-06 19:15:56 +0530

    changes

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use(cookieParser());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: ["https://capstone-frontendpro.netlify.app/",
+      "http://localhost:3000"],
     credentials: true,
   })
 );
